40435a09defad2ffcb46f21a1e064b85254d88d3
blackrose9 <sarah.mwendwa@gmail.com>
2020-02-18 15:20:07 +0300

    added routing for animal-form.hbs, and added new type field in Animal class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,12 +1,44 @@
-public class Animal {
-    private int mId;
-    private String mName;
-    private String mAge;
-    private String mHealth;
+import java.util.ArrayList;
 
-    public Animal(String mName, String mAge, String mHealth) {
-        this.mName = mName;
-        this.mAge = mAge;
-        this.mHealth = mHealth;
+public class Animal {
+    private int id;
+    private String type;
+    private String name;
+    private String age;
+    private String health;
+    //.....
+    private static ArrayList<Animal> instances = new ArrayList<Animal>();
+
+    public Animal(String mType, String mName, String mAge, String mHealth) {
+        this.type = mType;
+        this.name = mName;
+        this.age = mAge;
+        this.health = mHealth;
+        //.....
+        instances.add(this);
     }
+
+    public static ArrayList<Animal> getAll(){
+        return instances;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getType() { return type; }
+
+    public void setType(String type) { this.type = type; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getAge() { return age; }
+
+    public void setAge(String age) { this.age = age; }
+
+    public String getHealth() { return health; }
+
+    public void setHealth(String health) { this.health = health; }
 }
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import dao.Sql2oSightingDao;
+import model.Sighting;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -12,29 +14,36 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("public");
-        String connectionString = "jdbc:h2:~/tracker.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
-        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        String connectionString = "jdbc:postgresql://localhost:5432/tracker";
+        Sql2o sql2o = new Sql2o(connectionString, "postgres", "pg");
         Sql2oSightingDao sightingDao = new Sql2oSightingDao(sql2o);
-        Map<String, Object> model = new HashMap<String, Object>();
+
+        //instantiate some Animals to populate form
+        Animal animal = new Animal("Endangered", "Dodo", "26", "healthy");
+        Animal animal2 = new Animal("Endangered", "White Rhino", "59", "okay");
+        Animal animal3 = new Animal("Normal", "Chicken", "1", "healthy");
 
         get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/view_sightings", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
             List<Sighting> sightings = sightingDao.getAll();
             model.put("sightings", sightings);
             return new ModelAndView(model, "view-sightings.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/record_sighting/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Animal> animals = Animal.getAll();
+            model.put("animals", animals);
             return new ModelAndView(model, "sighting-form.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/record_sighting", (request, response) -> {
             String name = request.queryParams("name");
-            String age = request.queryParams("age");
-            String health = request.queryParams("health");
             String location = request.queryParams("location");
             String ranger = request.queryParams("ranger");
 
@@ -45,8 +54,22 @@ public class App {
             return null;
         }, new HandlebarsTemplateEngine());
 
-//        post("/record_animal", (request, response) -> {
-//
-//        }, new HandlebarsTemplateEngine());
+        get("/record_animal/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animal-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/record_animal", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String type = request.queryParams("type");
+            String name = request.queryParams("name");
+            String age = request.queryParams("age");
+            String health = request.queryParams("health");
+
+            Animal newAnimal = new Animal(type, name, age, health);
+
+            response.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
     }
 }
